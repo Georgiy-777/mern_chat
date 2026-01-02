d209b5015ee5c2b4abcf83578705fc536d07618f
Yuri Demyrov <yuriy.demirov@difreight.com>
2026-01-03 00:00:48 +0200

    init

---
+++ services/users/dist/config/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+const connectDb = async () => {
+    const url = process.env.MONGO_URL;
+    if (!url) {
+        throw new Error('MONGO_URL is not define');
+    }
+    try {
+        await mongoose.connect(url, {
+            dbName: "Chatappmicroservices"
+        });
+        console.log('Connected to MongoDb');
+    }
+    catch (error) {
+        console.error('Failed to connected to MongoDb');
+        process.exit(1);
+    }
+};
+export default connectDb;
+//# sourceMappingURL=db.js.map
---
+++ services/users/dist/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.js';
+dotenv.config();
+import connectDb from './config/db.js';
+import { redisClient } from './config/redis.js';
+connectDb();
+redisClient.connect().then(() => console.log(`Redis connected`)).catch(console.error);
+const app = express();
+app.use('api/v1', userRouter);
+const port = process.env.PORT;
+app.listen(port, () => {
+    console.log(`Server running on port :${port}`);
+});
+//# sourceMappingURL=index.js.map
